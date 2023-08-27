f61ecd5917a9019068061df7c60b0041013adb85
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2023-08-27 13:45:20 +0000

    Refactoring terraform installer to user apt_source.

--- installers/src/installers/terraform.sh
+++ installers/src/installers/terraform.sh
@@ -7,15 +7,10 @@
 
 source lib/common.sh
 
-# Installing prerequisites
-apt_install ca-certificates curl gnupg lsb-release
+apt_install_prerequisites
+apt_add_source -n hashicorp \
+    -d -k "https://apt.releases.hashicorp.com/gpg" \
+    -r "https://apt.releases.hashicorp.com" \
+    -- "$(lsb_release -cs) main"
 
-# Installing Terraforms's official GPG key
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/hashicorp-archive-keyring.gpg
-
-# Setting up the repository
-echo "deb [signed-by=/etc/apt/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-apt_clear_package_index
 apt_install terraform
