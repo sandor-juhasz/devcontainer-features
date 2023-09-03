3bcae6c13e475c01282d72736344460bb47e57ee
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2023-09-03 06:13:09 +0200

    Adding utilities preinstaller feature

---
+++ devcontainer-features/src/utilities/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# export INSTALL_AWS_CDK="${INSTALLAWSCDK:-"false"}"
+# export INSTALL_AWS_SAM_CLI="${INSTALLAWSSAMCLI:-"false"}"
+
+cd installers
+./install dotfiles-utilities
