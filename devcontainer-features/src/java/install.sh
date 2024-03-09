acb1f86bbee6483eb77d3ebcf9574b9ab709b415
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2024-03-09 12:01:07 +0100

    Adding iJava Jupyter kernel support to the Java feature

--- devcontainer-features/src/java/install.sh
+++ devcontainer-features/src/java/install.sh
@@ -4,6 +4,7 @@ set -e
 export JAVA_VERSION="${JAVAVERSION:-""}"
 export INSTALL_MAVEN="${INSTALLMAVEN:-"true"}"
 export MAVEN_VERSION="${MAVENVERSION:-""}"
+export INSTALL_IJAVA_KERNEL="${INSTALLIJAVAKERNEL:-"true"}"
 
 cd installers
 ./install java-development-tools
