ef8d872b5b0bf0a1f8f3f93b7aa1423e7512fb91
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2024-03-29 05:44:21 +0000

    Adding Oracle development tools feature

---
+++ devcontainer-features/src/oracle/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+export INSTALL_INSTANTCLIENT="${INSTALLINSTANTCLIENT:-true}"
+export INSTANTCLIENT_TYPE="${INSTANTCLIENTTYPE:-basic-light}"   # "basic" or "basic-light"
+export INSTALL_SQLPLUS="${INSTALLSQLPLUS:-true}"
+export INSTALL_TOOLS="${INSTALLINSTANTCLIENTTOOLS:-true}"
+export INSTALL_SDK="${INSTALLINSTANTCLIENTSDK:-false}"
+export INSTALL_JDBC="${INSTALLINSTANTCLIENTJDBC:-false}"
+export INSTALL_ODBC="${INSTALLINSTANTCLIENTODBC:-false}"
+
+cd installers
+./install oracle-development-tools
