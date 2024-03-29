9f427596622d56aca577e91f02f139b4d2686437
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2024-03-29 06:36:35 +0000

    Adding SQLcl installer

--- devcontainer-features/src/oracle/install.sh
+++ devcontainer-features/src/oracle/install.sh
@@ -8,6 +8,7 @@ export INSTALL_TOOLS="${INSTALLINSTANTCLIENTTOOLS:-true}"
 export INSTALL_SDK="${INSTALLINSTANTCLIENTSDK:-false}"
 export INSTALL_JDBC="${INSTALLINSTANTCLIENTJDBC:-false}"
 export INSTALL_ODBC="${INSTALLINSTANTCLIENTODBC:-false}"
+export INSTALL_SQLCL="${INSTALLSQLCL:-false}"
 
 cd installers
 ./install oracle-development-tools
