4093a1be70970a02f842b9b717db96a5a7e33083
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2024-04-03 15:04:26 +0000

    Adding jmeter option to the java feature

--- devcontainer-features/src/java/install.sh
+++ devcontainer-features/src/java/install.sh
@@ -5,6 +5,7 @@ export JAVA_VERSION="${JAVAVERSION:-""}"
 export INSTALL_MAVEN="${INSTALLMAVEN:-"true"}"
 export MAVEN_VERSION="${MAVENVERSION:-""}"
 export INSTALL_IJAVA_KERNEL="${INSTALLIJAVAKERNEL:-"true"}"
+export INSTALL_JMETER="${INSTALLJMETER:-"false"}"
 
 cd installers
 ./install java-development-tools
