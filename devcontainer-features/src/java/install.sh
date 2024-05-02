0910d457b3366e05ebbd0df2af9dc09110776d7b
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2024-05-02 08:11:01 +0000

    Extending the java feature with profiling tools

--- devcontainer-features/src/java/install.sh
+++ devcontainer-features/src/java/install.sh
@@ -6,6 +6,8 @@ export INSTALL_MAVEN="${INSTALLMAVEN:-"true"}"
 export MAVEN_VERSION="${MAVENVERSION:-""}"
 export INSTALL_IJAVA_KERNEL="${INSTALLIJAVAKERNEL:-"true"}"
 export INSTALL_JMETER="${INSTALLJMETER:-"false"}"
+export INSTALL_VISUALVM="${INSTALLVISUALVM:-"false"}"
+export INSTALL_JDK_MISSION_CONTROL="${INSTALLJDKMISSIONCONTROL:-"false"}"
 
 cd installers
 ./install java-development-tools
