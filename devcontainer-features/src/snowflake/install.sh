6e17f97ce1dbd83968f5ec6be8842af9d80a0f76
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2024-08-08 15:15:32 +0000

    Adding Snowflake CLI to the Snowflake feature

--- devcontainer-features/src/snowflake/install.sh
+++ devcontainer-features/src/snowflake/install.sh
@@ -3,6 +3,7 @@ set -e
 
 export INSTALL_SNOWSQL="${INSTALLSNOWSQL:-"true"}"
 export SNOWSQL_VERSION="${SNOWSQLVERSION:-"1.3.1"}"
+export INSTALL_SNOWFLAKE_CLI="${INSTALLSNOWFLAKECLI:-"false"}"
 
 cd installers
 ./install snowflake-development-tools
