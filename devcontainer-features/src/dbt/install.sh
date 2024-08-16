553f2d723cd8d58dfbfb250c9bdf04b8954948aa
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2024-08-16 08:29:06 +0200

    dbt: dbt cloud cli installer

--- devcontainer-features/src/dbt/install.sh
+++ devcontainer-features/src/dbt/install.sh
@@ -4,5 +4,10 @@ set -e
 export INSTALL_DBT_CORE="${INSTALLDBTCORE:-"true"}"
 export DBT_CORE_PLUGINS="${DBTCOREPLUGINS:-""}"
 
+
+export INSTALL_DBT_CLOUD_CLI="${INSTALL_DBT_CLOUD_CLI:-"false"}"
+export DBT_CLOUD_CLI_VERSION="${DBT_CLOUD_CLI_VERSION:-"0.38.10"}"
+export DBT_CLOUD_CLI_ALIAS="${DBT_CLOUD_CLI_ALIAS:-"dbt"}"
+
 cd installers
 ./install dbt-development-tools
